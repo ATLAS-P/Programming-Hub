366a3ad160e4569253c99da5705669ee47bf1876
RikMuld <rikmuld@gmail.com>
2016-11-02 14:45:25 +0100

    hotfix

--- server/autograder/AutoGrader.ts
+++ server/autograder/AutoGrader.ts
@@ -37,8 +37,8 @@ import init = IOMap.applyWithInput
 const inIsOut = a => ioTest(a, (i, o) => new Tuple(i == o, "Unexpected output, received: '" + o + "', expected: '" + i + "'."))
 const optimalGuess = a => ioTest(a, (i, o) => {
     if(o == -1) return new Tuple(false, "Your AI was not able to guess the result within 500 guesses.")
-    const bound = Math.floor(Math.log2(i[0])) + 1
-    return new Tuple(o <= bound, "Your result was not optimal. Your AI needed " + o + " tries. Optimal result was less than: " + bound + " tries.")
+    const bound = Math.ceil(Math.log2(i[0])) + 1
+    return new Tuple(o <= bound, "Your result was not optimal. Your AI needed " + o + " tries. Optimal result should be less or equal to: " + bound + " tries.")
 })
 const expected = data => a => dataTest(a, data, (a, b) => new Tuple(a == b, "Unexpected output, received: '" + a + "', expected: '" + b + "'."))
 const expectedF = (data, f) => a => dataTest(a, data, (a, b) => f(a, b))
@@ -164,7 +164,7 @@ export namespace Runners {
     function pythonSpawner<In, A, Out>(z: A, onData: (out: A, data: string, stdin: stream.Writable) => A, putInput: (stdin: stream.Writable, inn: In, running: () => boolean) => void | A, finalizeOutput: (a: A) => Out = ((a:A) => a as any as Out)): Spawn<In, Out> {
         return (filename: string) => (s: In) => new Future<Out>((resolve, reject) => {
             let running = true
-            let py = process.spawn("python", ['uploads/' + filename])
+            let py = process.spawn("python3", ['uploads/' + filename])
             let output = z
 
             py.stdout.on('data', function (data) {
@@ -225,7 +225,11 @@ export namespace Runners {
 
         export const guessRunner = pythonSpawner<number[], Tuple<number, number>, number>(new Tuple(0, 0), (out, data, stdin) => {
             const guess = getFirstNumber(data, -1)
-            if (out._2 > 500) return out.map_2(a => -1)
+            if (out._2 > 500) {
+                stdin.write("c" + BREAK)
+                stdin.end()
+                return out.map_2(a => -1)
+            }
             if (guess > out._1) stdin.write("l" + BREAK)
             else if (guess < out._1) stdin.write("h" + BREAK)
             else {
