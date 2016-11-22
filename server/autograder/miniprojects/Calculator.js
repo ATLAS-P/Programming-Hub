4a2546f37d3cdf710c0ebd1831d83489eabf8134
rikmuld <rikmuld@gmail.com>
2016-11-22 12:17:31 +0100

    calculator decimal check

--- server/autograder/miniprojects/Calculator.js
+++ server/autograder/miniprojects/Calculator.js
@@ -6,12 +6,23 @@ const Tuple_1 = require("../../functional/Tuple");
 var Calculator;
 (function (Calculator) {
     function init() {
-        const data = List_1.List.mk("7", "1+6", "4-7", "4*8", "4/5", "2^8", "1+2+3+4+5", "1*2*3*4*5", "(((1-2)-3)-4)-5", "1-(2-(3-(4-5)))", "(((1/2)/3)/4)/5", "1/(2/(3/(4/5)))", "(((5^4)^3)^2)^1", "4^(3^(2^1))", "9+(8*(4/3))+(2^7)", "(2^(1/2))-(8*4)", "(5+5*5*2*7)/(1+(2^4)*7)");
+        const data = List_1.List.mk("7", "1+6", "4-7", "4*8", "4/5", "2^8", "1+2+3+4+5", "1*2*3*4*5", "(((1-2)-3)-4)-5", "1-(2-(3-(4-5)))", "(((1/2)/3)/4)/5", "1/(2/(3/(4/5)))", "(((5^4)^3)^2)^1", "4^(3^(2^1))", "9+(8*(4/3))+(2^7)", "(2^(1/2))-(8*4)", "(5+(5*5*2*7))/(1+((2^4)*7))");
         const input = Projects_1.TestHelper.buildTest(data);
         const test = Projects_1.TestHelper.testDataCurry(List_1.List.apply(["7", "7", "-3", "32", "0.8", "256", "15", "120", "-13", "3", "0.008333333333333333",
             "1.875", "5.960464477539062e+16", "262144", "147.66666666666666", "-30.585786437626904", "3.1415929203539825"]), (out, data) => {
             const fixedOut = (out.endsWith(".0") ? out.substring(0, out.length - 2) : out);
-            const correct = fixedOut == data;
+            let correct = false;
+            if (data.indexOf("e") > 0) {
+                const dataParts = data.split("e");
+                const outParts = out.split("e");
+                if (dataParts.length == outParts.length && dataParts[1] == outParts[1]) {
+                    const length = Math.min(dataParts[0].length, 10);
+                    if (outParts[0].length >= length)
+                        correct = outParts[0].substring(0, length) == dataParts[0].substring(0, length);
+                }
+            }
+            else
+                correct = fixedOut == data;
             return new Tuple_1.Tuple(correct, correct ? "" : "Unexpected output, found '" + fixedOut + "', expected: '" + data + "'. This is case insensitive.");
         });
         const runner = Runners_1.Runners.PythonRunners.simpleIO;
