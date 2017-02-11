0b910e6d2c06ac23a6f043acd74755a0a5de240b
rikmuld <rikmuld@gmail.com>
2017-02-11 13:19:24 +0100

    added missing js files

---
+++ public/scripts/includes/dropzoneCustom.js
@@ -0,0 +1,44 @@
+let zone;
+Dropzone.options.uploadedFiles = {
+    createImageThumbnails: false,
+    parallelUploads: 1,
+    maxFilesize: 1,
+    init: dropzoneInit,
+    success: fileUploaded,
+    error: fileError
+};
+function dropzoneInit() {
+    zone = this;
+    zone.on("sending", sending);
+}
+function sending(file, xhr, formData) {
+    const ass = $("#uploadAssignment_title");
+    formData.append("assignment", ass.attr("assignment"));
+}
+function fileUploaded(file, response) {
+    zone.removeFile(file);
+    if (!response.success)
+        showError(response.err);
+    else {
+        $("#uploadAssignment .errorContainer").addClass("hidden");
+        $("#uploadAssignment .errors").html("");
+        const files = $("#uploadedFilesList");
+        const li = document.createElement("li");
+        li.innerText = file.name;
+        li.classList.add("list-group-item");
+        li.setAttribute("value", file.name);
+        files.append(li);
+        initListGroupItem($(li));
+        $(li).click();
+    }
+}
+function fileError(file, error) {
+    zone.removeFile(file);
+    showError(error);
+}
+function showError(error) {
+    $("#uploadAssignment .errorContainer").removeClass("hidden");
+    const li = document.createElement("li");
+    li.innerText = error;
+    $("#uploadAssignment .errors").append(li);
+}
